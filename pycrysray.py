a463cb15401e40081179a7ab52030b3346b92a25
Chih-hsiang Cheng <ahsiang.c@gmail.com>
2013-10-31 16:06:28 -0700

    add index of refraction and a new class Crystal

--- pycrysray.py
+++ pycrysray.py
@@ -1,4 +1,6 @@
 import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import sys
 arr = np.array
 sqrt = np.sqrt
@@ -7,22 +9,31 @@ sin = np.sin
 cos = np.cos
 
 ## Functions ===========================================
-def norm_vect(v):
+def unit_vect(v):
+    '''
+    Return the unit vector of direction v.
+    '''
     n = v.dot(v)
     if ( n > 0 ):
         return v / sqrt(n)
     return v
 
 def vect_norm(v):
+    '''
+    Return the norm of a vector |v|
+    '''
     return sqrt( v.dot(v) )
 
 def distance(x1,x2):
+    '''
+    Return the distance between two points |x1-x2|
+    '''
     return vect_norm( x2-x1 )
 
 def sine_angle(v1,v2,vtx=None):
     '''
-    sine of the angle formed by either three points v1, v2, and vtx with vtx as the 
-    vertex, or angle between two vectors v1 and v2.
+    sine of the angle formed by either three points v1, v2, and vtx with vtx as
+    the vertex, or angle between two vectors v1 and v2 if vtx=None.
     '''
     a,b = v1,v2
     if ( vtx != None ):
@@ -53,7 +64,7 @@ def rotate_vector(v, zhat):
     return arr(Rz*Ry* v.reshape(3,1)).reshape(3,)
 
 ## Geometries ===================================================
-def rect_prism(center,dim, random_reflect, diffuse_reflect, diffuse_sigma ):
+def rect_prism(center, dim, random_reflect, diffuse_reflect, diffuse_sigma ):
     '''
     Return a rectangular prism whose edges are parallel to the axes.
     *center* is the coordinate of the center of gravity
@@ -85,21 +96,20 @@ class Photon:
     wavelength : wavelength
     mfp : Mean free path
     '''
-    def __init__(self, x=None, dir=None, wavelength=None, mfp=1e9):
+    def __init__(self, x, dir, wavelength=None, mfp=1e9):
         zero3= np.zeros(3)
-        vx = arr([1.0,0.0,0.0], dtype='Float64')
-        if ( x == None ):  self.x = zero3
-        else: self.x = arr(x, dtype='Float64')
-        if ( dir == None ):  self.dir = vx
-        else: self.dir = norm_vect(arr(dir))
+        self.x = arr(x, dtype='Float64')
+        self.dir = unit_vect(arr(dir))
         if ( wavelength == None ): self.wavelength = 400e-9   # in meter
         else: self.wavelength = wavelength
         self.alive = True
+        self.detected = False
         self.pathlength = 0.0
         self.n_reflects = 0
         self.vertices= [x]
-        self.mother_box= arr([10]*3)
+        self.mother_box= arr([100.,100.,100.])
         self.mfp= mfp
+        self.deposit_at= None   # At which sensor
 
     def advance(self, d):
         '''
@@ -119,44 +129,70 @@ class Photon:
         '''
         return self.x + d* self.dir
 
-    def reflect(self, plane=None, normal=None, random_reflect=0, diffuse_reflect=0, diffuse_sigma=0):
+    def reflect(self, plane, sensors=None):
         '''
         Change direction as it reflects at a surface and add this position to 
-        the list of vertices. If a plane is given, normal and reflectivity
-        will be taken from the plane. Arguments will be ignored.
-
-        random_reflect: probability of random reflection
-        diffuse_reflect: probability of diffuse reflection
-        diffuse_sigma: width (sigma) in degrees of diffuse reflection
+        the list of vertices.
+        Return the plane the photon is on.
+        *plane*: The plane the photon is reflecting on
+        *sensors*: a list of Plane object that represents the sensors
         '''
-        if ( plane==None and normal==None ):
-            raise TypeError('Need either plane or normal')
+        ## Surface property
+        normal= plane.normal
+        rndrf= plane.random_reflect
+        difrf= plane.diffuse_reflect
+        difsig= plane.diffuse_sigma * np.pi/180.0
+        ifr_in= plane.idx_refract_in
+        ifr_out= plane.idx_refract_out
 
-        rndrf= random_reflect
-        difrf= diffuse_reflect
-        difsig= diffuse_sigma * np.pi/180.0
-        if ( plane!= None):
-            normal= plane.normal
-            rndrf= plane.random_reflect
-            difrf= plane.diffuse_reflect
-            difsig= plane.diffuse_sigma * np.pi/180.0
+        insensor= None
+        for sp in sensors:
+            # If the photon is inside the sensor area, change surface property
+            if sp.contain( self.x ):
+                insensor= sp
+                normal= sp.normal
+                rndrf= sp.random_reflect
+                difrf= sp.diffuse_reflect
+                difsig= sp.diffuse_sigma * np.pi/180.0
+                ifr_in= sp.idx_refract_in
+                ifr_out= sp.idx_refract_out
+                break
 
-        normal = norm_vect(normal)
-
+        normal = unit_vect(normal)
         # normal must point inward, i.e., if the photon direction is in the
         # same hemisphere as the normal, flip normal
         if ( normal.dot( self.dir ) > 0 ):
             normal = -normal
 
         self.vertices.append( self.x )
-        self.dir = norm_vect(self.dir)
+        self.dir = unit_vect(self.dir)
 
-        # Survive or not
-        rn = np.random.random_sample()
-        if ( rn >= rndrf+difrf ): # not reflected
-            self.alive = False
+        # Total reflection or not?
+        costh= normal.dot(self.dir)
+        sinth= np.sqrt(1- costh**2)
+        randomed= False
+        diffused= False
+        if sinth > ifr_out/float(ifr_in):  ## total reflection
+            randomed= False
+            diffused= True
+        else:
+            rn= np.random.random_sample()
+            if rn >= rndrf+difrf: # not reflected
+                self.alive = False
+                randomed= False
+                diffused= False
+            elif rn >= difrf:  #  random reflection
+                randomed= True
+                diffused= False
+            else:    # diffused reflection
+                randomed= False
+                diffused= True
 
-        elif ( rn >= difrf ):   # random (not diffuse)
+        if not self.alive and insensor is not None:
+            self.deposit_at= insensor
+
+        # How is it reflected
+        if self.alive and randomed:   # random reflection
             xphi = np.random.uniform(-np.pi, np.pi)
             xcth = np.random.uniform(0,0.999)   # cos theta
             xsth = sqrt(1-xcth**2)
@@ -165,10 +201,9 @@ class Photon:
             # rotate the vector so that its z axis points to normal
             self.dir = rotate_vector( vrand, normal)
 
-        else: # diffuse
+        if self.alive and diffused: # diffuse
             # specular reflection
             spec_dir = self.dir - 2*self.dir.dot(normal) * normal
-            
             while ( True ):
                 # random phi
                 xphi = np.random.uniform(-np.pi, np.pi)
@@ -184,7 +219,6 @@ class Photon:
                 if ( vrand.dot(normal) > 1e-6 ):
                     self.dir = vrand
                     break
-            pass
         
         if ( self.alive ):
             self.n_reflects += 1
@@ -200,7 +234,8 @@ class Photon:
 
     def nearest_plane(self, plist):
         '''
-        Return the distance and the plane that this photon will hit the first
+        Return the index of the next plane and the distance to that plane
+        (index, distance)
         '''
         dmin = 1e9
         imin = None
@@ -212,7 +247,7 @@ class Photon:
                 imin = i
         return imin, dmin
 
-    def propagate(self, plist, black_regions=[], verbose=False):
+    def propagate(self, crystal, verbose=False):
         '''
         Propagating this photon (A recursive function).
         Return the plane this photon dies on.
@@ -229,26 +264,27 @@ class Photon:
             return None
 
         # find the next plane
-        imin,dmin= self.nearest_plane(plist)
+        imin,dmin= self.nearest_plane(crystal.planes)
         if ( imin == None ):  # did not find any plane
             self.alive = False
             if ( verbose ): print 'Cannot find next plane'
             return None
         if ( verbose ):
             print 'path to next plane is', dmin
-            plist[imin].print_prop()
+            crystal.planes[imin].print_properties()
         # advance photon
         self.advance(dmin)
         if ( not self.alive ): return None
-        # check if it hits black boxes
-        for box in black_regions:
-            if ( box.contain(self.x ) ):
-                self.alive = False
-                return box
 
         # reflect on this plane
-        self.reflect(plist[imin])
-        return plist[imin]
+        self.reflect(crystal.planes[imin], crystal.sensors)
+
+        if not self.alive:
+            return crystal.planes[imin]
+
+        return self.propagate(crystal, verbose)
+
+
 
 ###################################################################
 class Plane:
@@ -256,20 +292,26 @@ class Plane:
     A class describing a flat plane in 3-dim space, defined by an array of 
     corners (3-vector). Only convex polygons are allowed.
     
-    corners : an array of space points (arrays of three floats)
-    random_reflect : probability of random reflection (angle uniform over (-90,90) degrees.)
-    diffuse_reflect : probability of diffuse reflection (angle in Gaussian distribution)
-    
-    diffuse_sigma : the sigma of the Gaussian.
+    *corners* : an array of space points (arrays of three floats). So the shape
+                of corners must be (n,3),  n>=3
+    *random_reflect* : probability of random reflection (angle uniform over 
+        (-90,90) degrees.)
+    *diffuse_reflect* : probability of diffuse reflection (angle in Gaussian 
+        distribution)
+    *diffuse_sigma* : the sigma of the Gaussian.
+    *idx_refract_in* : index of refraction on this side of the plane
+    *idx_refract_out* : index of refraction on the other side of the plane
+    *sensitive* : Is this plane a sensitive object (photo sensor) or not.
     '''
-    def __init__(self, corners, random_reflect=0.10, diffuse_reflect=0.90, diffuse_sigma=22, sensitive=False ):
+    def __init__(self, corners, random_reflect=0.10, diffuse_reflect=0.90, diffuse_sigma=22, idx_refract_in=1, idx_refract_out=1, sensitive=False ):
 
         shape = corners.shape
         if ( shape[0]<3 or shape[1]!=3 ): # corners is a an array of shape (n,3), n>=3
             raise TypeError('corners in %s(corners) must be of shape (n,3) with n>=3. But it is (%d,%d)' % ( self.__class__.__name__, shape[0], shape[1])) 
         self.corners = corners
+
         # Use the two edges at corners[0] to define its normal vector
-        self.normal= norm_vect( np.cross(corners[-1]-corners[0], corners[1]-corners[0]) )
+        self.normal= unit_vect( np.cross(corners[-1]-corners[0], corners[1]-corners[0]) )
         # Center of gravity
         self.cog = np.average( corners, axis=0 )
         self.tolerance= 1e-6
@@ -283,9 +325,12 @@ class Plane:
         #   within sigma = diffuse_sigma 
         self.diffuse_reflect = diffuse_reflect
         self.diffuse_sigma = diffuse_sigma
+        #  Index of reflection
+        self.idx_refract_in = idx_refract_in
+        self.idx_refract_out = idx_refract_out
 
         # base vectors
-        self.u = norm_vect( corners[0]- self.cog )
+        self.u = unit_vect( corners[0]- self.cog )
         self.v = np.cross( self.normal, self.u )
         ## Check none of the corners are too close
         for i in xrange(shape[0]-1):
@@ -318,10 +363,14 @@ class Plane:
         '''
         Return a point on this plane given the parameters (a,b), using
         the first and the last edges for the basis.
+        p = a*u + b*v
         '''
         return a*self.u+b*self.v
 
-    def print_prop(self):
+    def print_properties(self):
+        '''
+        Print out the property of this plane
+        '''
         print 'Plane:'
         print '  corners:'
         for c in self.corners:
@@ -330,6 +379,8 @@ class Plane:
         print '  random_reflect =', self.random_reflect
         print '  diffuse_reflect =', self.diffuse_reflect
         print '  diffuse_sigma =', self.diffuse_sigma
+        print '  index of refraction in = ', self.idx_refract_in
+        print '  index of refraction out = ', self.idx_refract_out
 
     def on_plane(self, x):
         '''
@@ -342,7 +393,7 @@ class Plane:
     
     def contain(self, x):
         '''
-        Return True if the position x is inside the polygon
+        Return True if the position x is inside the 2D polygon
         '''
         if ( not self.on_plane(x) ): return False
         nrm = np.cross( self.corners[-1]-x, self.corners[0]-x )
@@ -352,6 +403,32 @@ class Plane:
                 return False
                 
         return True
+
+
+
+###################################################################
+class Crystal:
+    '''
+    Class crystal. Defined by a list of Planes, and a list of sensors
+    Ex.
+       crystal_1= Crystal([p1, p2, p3, p4, p5, p6], [s1, s2])
+        pi and si are instances of Plane
+    '''
+    def __init__(self, planes, sensors):
+        self.planes= planes
+        self.sensors= sensors
+
+    def draw(self, ax=None):
+        '''
+        Draw the crystal in 3D axis
+        '''
+        if ax is None:
+            ax= plt.gca()
+        for p in list(self.planes)+list(self.sensors):
+            pts= np.concatenate((p.corners,[p.corners[0]]))
+            color='b'
+            if p.sensitive: color='r'
+            ax.plot(pts[:,0],pts[:,1],pts[:,2], color=color)
 
 
 ###################################################################
